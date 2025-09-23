8ec173d9209953fd20286dc0e4304b8936320a80
Fedorchenko Roman <dgbzznbx@gmail.com>
2025-09-23 22:13:33 +0300

    update

--- pr_2/py_pr_2_1.py
+++ pr_2/py_pr_2_1.py
@@ -14,14 +14,14 @@ if task==1:
     print("Z=", (m**0.5-n**0.5)/m)
 elif task==2:
     print("Task 2")
-    while not 0<=n:
+    while not n>0:
         n=int(input("Input your natural n (n>=0): "))
-        mass=[]
-        for i in range(1,n):
-            if(n%i==0):
-                mass.append(i)
-        if sum(mass)==n:
-            print(n, "is perfect number")
-            print("Divisors of", n, "are:", mass)
-        else:
-            print(n, "is not perfect number")
+    mass=[]
+    for i in range(1,n):
+         if(n%i==0):
+             mass.append(i)
+    if sum(mass)==n:
+         print(n, "is perfect number")
+         print("Divisors of", n, "are:", mass)
+    else:
+         print(n, "is not perfect number")
