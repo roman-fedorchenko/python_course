2fbf71b0ef5d185b111cf36d93ab248884795af0
Fedorchenko Roman <151760780+roman-fedorchenko@users.noreply.github.com>
2025-09-23 22:09:11 +0300

    Update py_pr_2_2.py

--- pr_2/task_2/py_pr_2_2.py
+++ pr_2/task_2/py_pr_2_2.py
@@ -1,11 +1,11 @@
 from f import f
 print("Variant 20 of practic 2. Task 2")
 n=-1
-while not (n>0):
+while not n>0:
     n=int(input("Input your natural n (n>=0): "))
-    mass, p = f(n)
-    if p:
-        print(n," is a perfect number")
-        print("Divisors of", n, "are:", mass)
-    else:
-        print(n," is not a perfect number")
+mass, p = f(n)
+if p:
+    print(n," is a perfect number")
+    print("Divisors of", n, "are:", mass)
+ else:
+    print(n," is not a perfect number")
